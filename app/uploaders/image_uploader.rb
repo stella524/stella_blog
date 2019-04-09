24e1ae01b267f6c40ee4a06e2e59a03d2230cc92
EC2 Default User <ec2-user@ip-172-31-22-235.ap-southeast-1.compute.internal>
2019-04-09 05:23:04 +0000

    done the index

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -32,6 +32,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # version :thumb do
   #   process resize_to_fit: [50, 50]
   # end
+  # version :thumb do
+  #   process resize_to_fit: [50, 50]
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
