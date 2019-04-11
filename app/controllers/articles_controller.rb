2a593260bd690232d4caeb7e3d394d5d105de22d
EC2 Default User <ec2-user@ip-172-31-22-235.ap-southeast-1.compute.internal>
2019-04-11 05:21:07 +0000

    layout change

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
   before_action :find_article, only:[:show,:edit,:update,:destroy]
   
   def index
-    @articles = Article.all.order(created_at: :desc)
+    @articles = Article.all.order(created_at: :desc).page(params[:page]).per(9)
   end
 
   def show
